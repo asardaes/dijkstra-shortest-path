800189f1f973fe17dd1ffdcf18cd1bab03f1724c
asardaes <alexis.sarda@gmail.com>
2016-04-23 15:35:00 +0200

    Test example

--- main.cpp
+++ main.cpp
@@ -6,26 +6,21 @@
 using namespace std;
 
 using vert_t = string;
-using weight_t = float;
+using weight_t = int;
 
 int main() {
-    //Graph<vert_t, weight_t> graph(vector<vert_t> {0, 1}, true);
-    Graph<vert_t, weight_t> graph(vector<vert_t> {"Me", "You", "He", "She"}, false);
+    Graph<vert_t, weight_t> graph(vector<vert_t> {"S", "A", "B", "C", "D", "E", "F", "G", "T"}, true);
+
+    graph.add_edge("S", vector<vert_t> {"A", "B", "D"}, vector<weight_t> {4, 3, 7});
+    graph.add_edge("A", "C", 1);
+    graph.add_edge("B", vector<vert_t> {"S", "D"}, vector<weight_t> {3, 4});
+    graph.add_edge("C", vector<vert_t> {"D", "E"}, vector<weight_t> {3, 1});
+    graph.add_edge("D", vector<vert_t> {"E", "F", "T"}, vector<weight_t> {1, 5, 3});
+    graph.add_edge("E", vector<vert_t> {"G", "T"}, vector<weight_t> {2, 4});
+    graph.add_edge("G", vector<vert_t> {"E", "T"}, vector<weight_t> {2, 3});
+    graph.add_edge("T", "F", 5);
 
     cout << "Number of vertices = " << graph.size() << endl << endl;
-
-    graph.print();
-
-    //graph.add_edge(0, 1, 1);
-    //graph.add_edge(1, 0, 2);
-
-    graph.add_edge("Me", "You", 1);
-    graph.add_edge("You", "Me", 2);
-
-    graph.add_edge("He", vector<vert_t> {"Me", "You"}, vector<weight_t> {3.5, 4.5});
-    graph.add_edge("She", vector<vert_t> {"Me", "You", "He"}, vector<weight_t> {5.5, 6.5, 7.5});
-
-    cout << endl << "After insertion:" << endl;
     graph.print();
 
     return 0;
